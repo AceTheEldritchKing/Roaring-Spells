be74965d762f6f16f89608eab6f7e558ed386601
Ace The King <solcat0414@gmail.com>
2026-08-19 00:12:51 -0400

    Changelog:
    Added planned spell list

--- src/main/java/net/acetheeldritchking/roaring_knight_iss/registries/RKSpellRegistries.java
+++ src/main/java/net/acetheeldritchking/roaring_knight_iss/registries/RKSpellRegistries.java
@@ -23,8 +23,13 @@ public class RKSpellRegistries {
     // Knight's Edge - Conjure a group of shadow blades with down upon a selected area. Blades cause hit entities to take more Eldritch damage
     public static final Supplier<AbstractSpell> KNIGHTS_EDGE = registerSpell(new KnightsEdgeSpell());
 
-    // Star Storm
-    public static final Supplier<AbstractSpell> STAR_STORM = registerSpell(new StarStormSpell());
+    // Star Storm ?
+
+    // Star Shrapnel
+
+    // Cutscene Merchant
+
+    // Swoon
 
 
     public static void register(IEventBus eventBus)
